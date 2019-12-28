9ce95ab1d143a3c8321105f739b1180de3bcc342
vlad095 <41382192+vlad095@users.noreply.github.com>
2019-12-28 06:49:54 +0100

    alpha version

--- scripts/cookie_notice.js
+++ scripts/cookie_notice.js
@@ -5,12 +5,12 @@
 /*-----------------------------------------------------*/
 
 $(document).ready(function() { 
-    if (localStorage.getItem('cookieSeen') !== 'shown') {
+    if(localStorage.getItem('cookieSeen') !== 'shown') {
        $('.cookie-banner').delay(3000).fadeIn(2000);    
     }
      
     $('.cookie-banner-close').click(function(){
-        if ($(this).data('clicked', true)) {
+        if($(this).data('clicked', true)) {
             $('.cookie-banner').fadeOut();
             localStorage.setItem('cookieSeen', 'shown');
         } 
--- scripts/nav_scroll.js
+++ scripts/nav_scroll.js
@@ -4,16 +4,16 @@
 /*-------------- Author: Vladimir Maric ---------------*/
 /*-----------------------------------------------------*/
 
-$(document).scroll(function () {
+$(document).scroll(function() {
     var scrollPosition = $(this).scrollTop();
     var screenWidth = $(window).width();
     
-    window.onload = function () {
+    window.onload = function() {
         $('#top-bar').fadeIn(1);
         document.getElementById("navbar").style.top = "0";
     };
     
-    if (scrollPosition === 0 && screenWidth > 990) {
+    if(scrollPosition === 0 && screenWidth > 990) {
         /* Shows top-bar if scrolled to top and screen width is bigger than 990px */
         $('#top-bar').fadeIn(1);
         document.getElementById("navbar").style.top = "32px";
